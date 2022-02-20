ebcbb6eb774072ed94936ad568653d30c18d7d80
Maxim Kazakov <kazakov.java@gmail.com>
2022-02-20 14:45:59 +0300

    L04-generics

---
+++ L04-generics/src/main/java/homework/Customer.java
@@ -0,0 +1,75 @@
+package homework;
+
+import java.util.Objects;
+
+public class Customer implements Comparable<Customer>, Cloneable {
+    private final long id;
+    private String name;
+    private long scores;
+
+    //todo: 1. в этом классе надо исправить ошибки
+
+    public Customer(long id, String name, long scores) {
+        this.id = id;
+        this.name = name;
+        this.scores = scores;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getScores() {
+        return scores;
+    }
+
+    public void setScores(long scores) {
+        this.scores = scores;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", scores=" + scores +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
+    @Override
+    public int compareTo(Customer customer) {
+        return Objects.equals(this.scores, customer.scores) ? 0 : this.scores > customer.scores ? 1 : -1;
+    }
+
+    @Override
+    public Customer clone() {
+        try {
+            Customer clone = (Customer) super.clone();
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
