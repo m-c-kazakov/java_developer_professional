72d22c956b92da350665307b2ff2e02723738fdd
Maxim Kazakov <kazakov.java@gmail.com>
2022-02-24 06:54:46 +0300

    L10-byteCodes Инициализация коллекции аннотированных методов при создании прокси; Реализация класса передается извне; Удалил сравнение интерфейсов, т.к. всегда проксируется раелазация определенного интерфейса

--- L10-byteCodes/src/main/java/org/logger/HomeWorkStarter.java
+++ L10-byteCodes/src/main/java/org/logger/HomeWorkStarter.java
@@ -2,7 +2,7 @@ package org.logger;
 
 public class HomeWorkStarter {
     public static void main(String[] args) {
-        TestLogging testLogging = IoC.createTestLogging();
+        TestLogging testLogging = IoC.createTestLogging(new TestLoggingImpl());
         testLogging.calculation(1);
         testLogging.calculation(2,3);
         testLogging.calculation(4,5,6);
