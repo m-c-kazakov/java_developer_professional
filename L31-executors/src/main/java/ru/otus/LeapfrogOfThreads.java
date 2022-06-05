735eb7f3a7b07eff56a1aeb22d5762a1fc3e4e84
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2022-06-05 11:00:15 +0300

    L31-executors Исправления по замечаниям

--- L31-executors/src/main/java/ru/otus/LeapfrogOfThreads.java
+++ L31-executors/src/main/java/ru/otus/LeapfrogOfThreads.java
@@ -1,31 +1,54 @@
 package ru.otus;
 
+import lombok.AccessLevel;
 import lombok.SneakyThrows;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 @Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class LeapfrogOfThreads {
+
+    static Queue<Integer> queue = new ArrayBlockingQueue<>(2);
 
     @SneakyThrows
     public static void main(String[] args) {
-        new Thread(LeapfrogOfThreads::execute).start();
-        new Thread(LeapfrogOfThreads::execute).start();
+        int firstThread = 0;
+        queue.add(firstThread);
+        int secondThread = 1;
+        queue.add(secondThread);
+        new Thread(()-> LeapfrogOfThreads.execute(firstThread)).start();
+        new Thread(()-> LeapfrogOfThreads.execute(secondThread)).start();
     }
 
     @SneakyThrows
-    public static synchronized void execute() {
+    public static synchronized void execute(Integer threadNumber) {
 
         for (int i = 1; i < 11; i++) {
             log.info("ThreadName={} Number={}", Thread.currentThread().getName(), i);
-            LeapfrogOfThreads.class.notifyAll();
-            LeapfrogOfThreads.class.wait();
+            waitExecute(threadNumber);
         }
 
         for (int i = 9; i > 0; i--) {
             log.info("ThreadName={} Number={}", Thread.currentThread().getName(), i);
+            waitExecute(threadNumber);
+        }
+        LeapfrogOfThreads.class.notifyAll();
+    }
+
+    @SneakyThrows
+    private static void waitExecute(Integer threadNumber) {
+
+        queue.poll();
+        queue.add(threadNumber);
+        while (!Objects.equals(queue.peek(), threadNumber)) {
             LeapfrogOfThreads.class.notifyAll();
             LeapfrogOfThreads.class.wait();
         }
-        LeapfrogOfThreads.class.notifyAll();
+
     }
 }
